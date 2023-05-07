f96d80e7880f70bc3e5e3cb4cc9f1c3c4e35c5fb
Malik <willis.malik.r@gmail.com>
2023-05-07 16:06:55 -0500

    Attacks model initialized along with appropiate properties

---
+++ server/src/models/Attacks.js
@@ -0,0 +1,25 @@
+const { sequelize, Model, DataTypes } = require("../db/config")
+
+class Attacks extends Model {}
+
+Attacks.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    title: DataTypes.STRING,
+    mojoCost: DataTypes.INTEGER,
+    staminaCost: DataTypes.INTEGER,
+    },
+
+    {
+        sequelize: sequelize,
+        modelName: "Attacks",
+        timestamps: false
+    })
+
+module.exports = {
+    Attacks
+}
