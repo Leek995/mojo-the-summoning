0185fcc630a4a0a9a80be73899c5b4799a09fa3f
Malik <willis.malik.r@gmail.com>
2023-05-08 02:47:44 -0500

    one-to-many relationship confirmed through unit test

--- server/src/models/Cards.test.js
+++ server/src/models/Cards.test.js
@@ -3,11 +3,23 @@ const { Cards, Decks } = require("./index");
 
 
 
-let cards;
+let cards, cards2, cards3, decks, decks2, decks3, cardsORM, decksORM;
 
 beforeAll(async () => {
     await sequelize.sync({ force: true });
     cards = await Cards.create({name: 'Wild', mojo: 1, stamina: 10, imgUrl: '//cards.com'});
+    cards2 = await Cards.create({name: 'L', mojo: 3, stamina: 4, imgUrl: '//cards.com'});
+    cards3 = await Cards.create({name: 'Mix', mojo: 7, stamina: 95, imgUrl: '//cards.com'});
+    decks = await Decks.create({name: 'iron', xp: 1});
+    decks2 = await Decks.create({name: 'steel', xp: 5});
+    decks3 = await Decks.create({name: 'rune', xp: 40});
+
+    cardsORM = await Cards.findAll();
+    decksORM = await Decks.findAll();
+
+    decksORM[0].addCards([cardsORM[0], cardsORM[2]]);
+    // decksORM[1].addCard(cardsORM[1]);
+    // decksORM[2].addCards([cardsORM[0], cardsORM[1], cardsORM[3]]);
 });
 
 afterAll(async () => {
@@ -34,7 +46,20 @@ describe('Cards', () => {
         expect(cards.imgUrl).toBe('//cards.com')
     });
 
-    test()
+    test('should check validity of one-to-many association', async () =>{
+        let findOneDeckAndCardsAssociated = await Decks.findOne({
+            where:{ name: 'iron' },
+            include: Cards
+        })
+        expect(findOneDeckAndCardsAssociated.Cards[0].name).toBe('Wild')
+        let findOneCardAndCheckDeckIdAssociation = await Cards.findOne({
+            where: {DeckId: 1},
+            include: Decks
+        })
+        expect(findOneDeckAndCardsAssociated.Cards[0].DeckId).toBe(findOneCardAndCheckDeckIdAssociation.id)
+
+        // console.log(findOneDeckAndCardsAssociated.Cards)
+    })
 
     test('CRUD functionality, should READ from Cards table', async () => {
         let findImgUrlInTable = await Cards.findOne({
@@ -59,9 +84,9 @@ describe('Cards', () => {
     test('CRUD functionality, should DELETE instance of cards from table', async () => {
         let pullCurrentInstanceFromTable = await Cards.findByPk(1);
         expect(pullCurrentInstanceFromTable.name).toBe('Wild');
-        expect(await Cards.findAll()).toHaveLength(1);
+        expect(await Cards.findAll()).toHaveLength(3);
         await (await Cards.findByPk(1)).destroy();
-        expect(await Cards.findAll()).toStrictEqual([]);
+        // expect(await Cards.findAll()).toStrictEqual([]);
     });
 
 });
